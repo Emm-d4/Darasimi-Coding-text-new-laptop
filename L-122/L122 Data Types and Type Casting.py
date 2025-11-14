e6e2fc16d6c8f93e7ad05b85fa79ca2359c3ae3b
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2025-11-14 16:45:15 +0000

    L122

---
+++ L-122/L122 Data Types and Type Casting.py
@@ -0,0 +1,34 @@
+# Assigning Diffrent Variables
+name = "Darasimi"
+age = 10
+is_student = True
+weight = 38.5
+
+# Priniting Diffrent Varibles and their Data Type
+print("original:")
+
+print("Name :",name)
+print("Data Type of Name is", type(name))
+
+print("Age :",age)
+print("Data Type of Age is", type(age))
+
+print("is_student :",is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("weight :",weight)
+print("Data Type of weight is", type(weight))
+
+print()
+print()
+
+# Type casting to convert to convert the datatype of variables
+print("After casting...")
+
+age = str(age)
+print("age after typecasting: ,"age)
+print("Data Type of age is", type(age))
+
+age = int(weight)
+print("weight after typecasting: ,"weight)
+print("Data Type of Weight is", type(Weight))
