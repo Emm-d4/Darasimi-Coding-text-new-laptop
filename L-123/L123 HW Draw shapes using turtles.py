e39b759546afa7f74211af093b32a5ddf08dc4c6
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2025-11-19 17:34:50 +0000

    L123

---
+++ L-123/L123 HW Draw shapes using turtles.py
@@ -0,0 +1,41 @@
+import turtle
+
+turtle.Screen().bgcolor("Gold")
+paper =turtle.Screen()
+paper.setup(700,500)
+
+pen = turtle.Turtle()
+
+# pentagone
+angle3 = 360/5
+pen.forward(100)
+
+pen.left(angle3)
+pen.forward(100)
+
+pen.left(angle3)
+pen.forward(100)
+
+#move position
+pen.penup()
+pen.left(120)
+pen.forward(200)
+
+#start to draw
+pen.pendown()
+
+# hexagone
+angle4 = 360/6
+pen.forward(100)
+pen.left(angle4)
+
+pen.forward(100)
+pen.left(angle4)
+
+pen.forward(100)
+pen.left(angle4)
+
+pen.forward(100)
+pen.left(angle4)
+
+turtle.done()
