7a7eaed737e83d6856287d41a6a904ee7df09fbf
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2025-12-12 16:32:57 +0000

    L-130

---
+++ L-130/L130 Patterns.py
@@ -0,0 +1,18 @@
+# Basic Star Patten
+print("Star Patten")
+
+for i in range(1,6):
+    for j in range(i):
+        print("*", end="")
+
+    print()
+
+print()
+
+#Inverted Star Pattern
+print("Inverted Star Pattern")
+for i in range(6,1,-1):
+    for j in range(i, 1, -1):
+        print("*", end="")
+
+    print()
