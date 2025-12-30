b9da23a50cf75613bb085c4534db32a4c86629d4
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2025-12-30 16:35:47 +0000

    L134

---
+++ L-134/L134 Read Operation - 1.py
@@ -0,0 +1,15 @@
+#open file and read its contents
+file = open("L-134/sample.txt",'r')
+print(file.read())
+file.close()
+
+#open file and read its beginning 8 characters
+file = open("L-134/sample.txt",'r')
+print("\n Read in parts \n")
+print(file.read(8))
+file.close()
+
+#append your name age in the file
+file = open("L-134/sample.txt",'a')
+file.write(" Hello.. my name is Darasimi")
+file.close()
