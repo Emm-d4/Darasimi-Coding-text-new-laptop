cf3291bed8b89cac1524bbe0b73dd77d89c583fa
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2026-01-06 15:46:14 +0000

    L135

---
+++ L-135/L135 HW File Handling - Operations Part 2.py
@@ -0,0 +1,18 @@
+#create a new file
+new_file = open("L-135/new-sample.txt", 'x')
+new_file.close()
+
+#check if a file exists
+import os
+
+print("Checking if my_file exists or not....")
+
+if os.path.exists("L-135/sample.txt"):
+    os.remove("L-135/sample.txt")
+else:
+    print("The file does not exists")
+
+#create a new if it doesn't
+my_file = open("L-135/sample.txt", "w")
+my_file.write("Hi I am Darasimi and I am 10 year old")
+my_file.close()
