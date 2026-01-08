18c5971f70541d62d95c0f2a38e97d24716a7b19
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2026-01-08 17:58:01 +0000

    L136

---
+++ L-136/L136 HW Basic Calculator Using Python.py
@@ -0,0 +1,20 @@
+print("Hello, welcome to this very basic calculator")
+print("Start by entering the two numbers for conducting calculation")
+
+add = 0
+sub = 0
+mul = 0
+div = 0
+
+fnum = int(input("Enter first number: "))
+snum = int(input("Enter second number: "))
+
+add = fnum + snum
+sum = fnum - snum
+mul = fnum * snum
+div = fnum / snum
+
+print("The addition of the two numbers is ", add)
+print("The subtraction of the two numbers is ", sub)
+print("The multiplation of the two numbers is ", mul)
+print("The division of the two numbers is ", div)
