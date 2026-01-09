40c61cc7f64ff2b42f955bc958faf212089d3fdf
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2026-01-09 18:50:35 +0000

    L137-L138

--- L-133/Loops.py
+++ L-133/Loops.py
@@ -1,1 +1,8 @@
-loop is when it is going over and over again
+num = 1
+sum = 0
+
+while(num<=10):
+    sum = sum + num
+    num = num + 1
+
+print("Sum of first 10 Natuarl Numbers :", sum)
---
+++ L137-138/HW.py
@@ -0,0 +1,22 @@
+import time
+
+score = 0
+
+print("Hello there, welcome to the General Knowledge Quiz")
+time.sleep(1.5)
+
+print("There are 10 questions, try to answer them all correctly")
+time.sleep(1.5)
+
+print("Choose A, B C, or D to answer!")
+time.sleep(1.5)
+
+print("Your all set. Good luck!")
+
+question_1 = input("1. What is the largest staduim in the world?\nA. spotify camp nou\nB. 1st of may\nC. old tranford\nD. 1st of may")
+
+question_1 = input("1. What is the biggest country?\nA. Russia\nB. Usa\nC. China\nD. Russia: ")
+
+question_1 = input("1. What is the smallest country?\nA. Uk\nB. Russia\nC. Vatican city\nD. Vatican city")
+
+question_1 = input("1. What country hase the best army\nA. Russia\nB. Uk\nC. Usa\nD. Usa")
