8c159d2c110ff19052b9d6e321fb89e5be5eb2de
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2026-01-29 16:58:04 +0000

    L127

--- L-127/HW.py
+++ L-127/HW.py
@@ -1,11 +1,34 @@
-class fruit:
+class CSStudent:
+ 
+    # Class Variable
+    stream = 'cse'            
+ 
+    # The init method or constructor
+    def __init__(self, roll):
+   
+        # Instance Variable   
+        self.roll = roll    
 
-    # Constructor
-    def __init__(self, name, color):
-        self.name = name
-        self.color = color
-
-# Object Creation
-apple = fruit('Apple', 'Red')
-# Calling Function
-print(apple.name)
+    def setAddress(self, address):
+        self.address = address
+     
+    # Retrieves instance variable   
+    def getAddress(self):   
+        return self.address  
+ 
+# Driver Code
+add = CSStudent(101)
+add.setAddress("Pune, Maharashtra")
+print(add.getAddress())  
+  
+# Objects of CSStudent class
+a = CSStudent(101)
+b = CSStudent(102)
+  
+print(a.stream)  # prints "cse"
+print(b.stream)  # prints "cse"
+print(a.roll)    # prints 101
+  
+# Class variables can be accessed using class
+# name also
+print(CSStudent.stream) # prints "cse"  
