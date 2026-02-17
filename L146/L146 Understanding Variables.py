c24a324ac236157b98be20c0081f7bc88e319147
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2026-02-17 16:35:42 +0000

    L146

---
+++ L146/L146 Understanding Variables.py
@@ -0,0 +1,13 @@
+#Creating diffrent variables
+x = 5
+y = "Darasimi"
+
+print(x)
+print(y)
+
+# User Input
+name = input("enter your name: ")
+print()
+
+print("Hello " + name)
+print("welcome to Codingal")
---
+++ L146/L146 Welcome to the World of Programming.py
@@ -0,0 +1,16 @@
+# is comments of my personal note to  help me understand the codes
+
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End aragument of Print Statement
+print("welcome to", end= "+")
