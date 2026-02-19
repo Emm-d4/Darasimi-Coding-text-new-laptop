17d10d9c199ea1be432cbed0d8206f79ba6a63ff
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2026-02-19 13:03:45 +0000

    L146

---
+++ L146/HW.py
@@ -0,0 +1,20 @@
+birthday1 = input ("when is your birthday, Michael? ")
+print()
+
+birthday2 = input("when is your birthday, Modesire? ")
+print()
+
+birthday3 = input("when is your birthday, Sameul? ")
+print()
+
+birthday4 = input("when is your birthday, Bolakunmi? ")
+print()
+
+birthday5 = input("when is your birthday, Adeoluwa? ")
+print()
+
+print("Michael's birthday is "+birthday1)
+print("Modesire's birthday is "+birthday2)
+print("Sameul's birthday is "+birthday3)
+print("Bolakunmi's birthday is "+birthday4)
+print("Adeoluwa's birthday is "+birthday5)
