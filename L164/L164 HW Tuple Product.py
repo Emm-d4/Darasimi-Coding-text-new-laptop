20db95f838b7c9383ccfebefca92f018c299fda7
Emm-d4 <akinyedeoluwaseunayo@gmail.com>
2026-04-21 16:44:50 +0100

    L164

---
+++ L164/L164 HW Tuple Product.py
@@ -0,0 +1,18 @@
+def mutiple_tuple(nums):
+    temp = list(nums)
+    product = 1
+
+    for x in temp:
+        product = product 
+
+    return product
+
+nums = (4, 3, 2, 12, -1, 180)
+print ("Original Tuple: ")
+print(nums)
+print()
+
+result = mutiple_tuple(nums)
+
+print("Product - multiplying all the numbers of the said tuple:",result)
+print()
